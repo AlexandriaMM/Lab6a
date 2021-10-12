8666ad7f03ad18b5b18316b4b87d7ccb1e1f178b
AlexandriaMM <89824931+AlexandriaMM@users.noreply.github.com>
2021-10-12 02:40:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+
+class Program {
+  public static void Main (string[] args) {
+    
+    int[] Celsius = new int[7];
+    int[] Farenheit = new int[7];
+    int sum = 0;
+    int average = 0;
+
+    for (int i = 0; i < 7; i++)
+    {
+      Console.WriteLine("Enter temperature in celsius");
+      Celsius[i] = Convert.ToInt32(Console.ReadLine());
+      Farenheit[i] = (Celsius[i] * 9/5) + 32;
+      sum = sum + Farenheit[i];
+    }
+
+    Console.WriteLine("Temperature in Farenheit");
+    
+    foreach (int n in Farenheit)
+    {
+      Console.WriteLine(n);
+    }
+
+    average = sum / 7;
+    Console.WriteLine("Average weekly tempurature is " + average + " degrees fahrenheit");
+  }
+}
